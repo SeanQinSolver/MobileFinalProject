a34372b67f6f96b0ffc06076cd323fa880da9c04
Seanqin86 <hector86>
2016-07-27 09:51:44 -0400

    new
    new

---
+++ RunRecipe/1233.h
@@ -0,0 +1,13 @@
+//
+//  1233.h
+//  RunRecipe
+//
+//  Created by QinShawn on 7/27/16.
+//  Copyright © 2016 Team1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _233 : UIViewController
+
+@end
---
+++ RunRecipe/1233.m
@@ -0,0 +1,37 @@
+//
+//  1233.m
+//  RunRecipe
+//
+//  Created by QinShawn on 7/27/16.
+//  Copyright © 2016 Team1. All rights reserved.
+//
+
+#import "1233.h"
+
+@interface _233 ()
+
+@end
+
+@implementation _233
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
